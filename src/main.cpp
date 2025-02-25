5ea5115cf6d54eaae63689bcfa9bc9bb464c8fd1
goooooseter <goooooseter@gmail.com>
2025-02-25 06:53:46 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int main() {
 	int a = 0;
 	int b = 0;
 	std::cin >> a >> b;
-	std::cout << "a + b = " << add(a, b) << std::endl;
-	std::cout << "a - b = " << sub(a, b) << std::endl;
+	std::cout << "Sum: " << add(a, b) << std::endl;
+	std::cout << "Difference: " << sub(a, b) << std::endl;
+	std::cout << "Product: " << mul(a, b) << std::endl;
 	return 0;
 }
