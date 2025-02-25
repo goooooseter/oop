03948760b844f0776514d4dcd350cafdc549475c
goooooseter <goooooseter@gmail.com>
2025-02-25 06:44:45 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main() {
+	int a = 0;
+	int b = 0;
+	std::cin >> a >> b;
+	std::cout << "a + b = " << add(a, b) << std::endl;
+	std::cout << "a - b = " << sub(a, b) << std::endl;
+	return 0;
+}
